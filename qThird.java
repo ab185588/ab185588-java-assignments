34dad775a7df1115d99179b756cdc10003a56de8
ab185588ncr <ab185588@ncr.com>
2022-12-09 01:53:59 +0530

    new
    code and .class files for question on printing patterns

---
+++ qThird.java
@@ -0,0 +1,16 @@
+public class qthird{
+	public static void main(String[] args){
+		int size = Integer.parseInt(args[0]);
+		int count = 1 ;
+		for(int i = 1 ; i <= size ; i++){
+			for(int j = size ; j > i ; j--){
+  				System.out.print(" ");
+			}
+			for(int k = 0 ; k < 2*i-1 ; k++){
+				System.out.print(count);
+			}
+			count++;
+			System.out.println(" ");
+		}
+	}
+}
