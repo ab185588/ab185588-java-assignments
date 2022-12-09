122d40216cbae250b0d81c7b9ec44edcc7274250
ab185588ncr <ab185588@ncr.com>
2022-12-09 11:40:02 +0530

    change from range to limit taxation rate

--- itSlab.java
+++ itSlab.java
@@ -1,19 +1,34 @@
 public class itSlab
-{
-	public static void main(String[] args) {
-		double sal = Double.parseDouble(args[0]);
-		if(sal<=40000) ;
-		else if(sal>40000 && sal <=80000 ){
-		    System.out.println("Your tax is : " + (sal-40000)*0.1);
-		}
-		else if(sal>80000 && sal <=150000){
-		    System.out.println("Your tax is : " + (sal-80000)*0.2 + 4000);
-		}
-		else if(sal>150000){
-		    System.out.println("Your tax is : " + (sal-150000)*0.3+12000);
+{	
+ 	public static void main(String[] args) {
+	double sal = Double.parseDouble(args[0]);
+		System.out.println(sal);
+		double tax = 0;
+		if(sal-80000<0) {
+			sal=0;
 		}
 		else{
-		    System.out.println("Please enter valid salary");
+			sal = sal-80000;
 		}
+		if(sal-80000<0){
+		    	tax += 0.1*(sal);
+		    	sal=0;
+		}
+		else{
+			tax += 8000;
+			sal = sal - 80000;
+			
+		}
+		if(sal-150000<0){
+		   	tax += 0.2*(sal);
+		   	sal=0;
+		}
+		else {
+		    tax += 30000;
+			sal = sal - 150000;
+		}
+		
+		tax += 0.3*sal;
+		System.out.println(tax);
 	}
 }
